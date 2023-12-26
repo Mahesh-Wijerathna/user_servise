54359b6154c3026edba45d6f5067c068b3db5e69
W.M.M.T. Wijerathna <138859871+Mahesh-Wijerathna@users.noreply.github.com>
2023-12-26 16:04:58 +0000

    lohin created partial

--- frontend_service/frontend/src/App.js
+++ frontend_service/frontend/src/App.js
@@ -1,94 +1,38 @@
 
 
-import React, { useState } from "react";
-import axios from "axios";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Home from './components/Home'; // Import the 'Home' component
 
 
 
 
 function App() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const isTesting = 0;
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    await axios.post("https://automatic-space-system-v6v6pxwv66wrhpvx6-4000.app.github.dev/api/v1/auth", {
-      medicalCenterRegistrationId: "123456789",
-      name: "Medical Center Name",
-      ownerName: "Owner Name",
-      address: "123 Main Street",
-      email: "test@example.com",
-      phone: "123-456-7890",
-      username: "testuser",
-      password: "testpassword",
-      nearestDestination: "City Hospital",
-      medicalCenterMedia: "http://example.com/image.jpg"
-      
-     
-      
-    }).then((response) => {
-      console.log("then worked");
-      console.log(response);
-      window.location.href = "/";
-    }).catch((error) => {
-      
-      if (error.response) {
-        
-        console.log("error in response");
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
-      } else if (error.request) {
-        console.log("error in request");
-        
-        console.log(error.request);
-        setName(error.request);
-      } else {
-        console.log("other error");
-        console.log('Error', error.message.toJSON());
-      }
-      // console.log(error.config);
-    });
+  
+  
+  if(isTesting)return (
 
-    setEmail("");
-    setPassword("");
-  };
-  return (
+    <>
+    
+    <h1>
+      hi
+    </h1>
 
-    <div className="login">
-      <form className="login__form" onSubmit={(e) => handleSubmit(e)}>
-        <h1>Login here 🚪</h1>
-        <input
-          type="name"
-          placeholder="Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <input
-          type="name"
-          placeholder="Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <input
-          type="email"
-          value={email}
-          placeholder="Email"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          value={password}
-          placeholder="Password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit" className="submit__btn">
-          Submit
-        </button>
-      </form>
+    </>
+
+  );
+  else return (
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          
+        </Routes>
+      </Router>
     </div>
-
   );
 
 
--- frontend_service/frontend/src/components/Home.js
+++ frontend_service/frontend/src/components/Home.js
@@ -8,29 +8,39 @@ const Home = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    await axios.post("http://localhost:3001/api/v1/buyers", {
-      name: name,
-      email: email,
-      password: password,
-      phone: "123-456-7890"
+    await axios.post("https://automatic-space-system-v6v6pxwv66wrhpvx6-4000.app.github.dev/api/v1/auth", {
+      medicalCenterRegistrationId: "123456789",
+      name: "Medical Center Name",
+      ownerName: "Owner Name",
+      address: "123 Main Street",
+      email: "test@example.com",
+      phone: "123-456-7890",
+      username: "testuser",
+      password: "testpassword",
+      nearestDestination: "City Hospital",
+      medicalCenterMedia: "http://example.com/image.jpg"
+      
+     
+      
     }).then((response) => {
+      console.log("then worked");
       console.log(response);
       window.location.href = "/";
     }).catch((error) => {
       
-
       if (error.response) {
         
-        console.log("error.response");
-        console.log(error.response.data.toJSON());
-        console.log(error.response.status.toJSON());
-        console.log(error.response.headers.toJSON());
+        console.log("error in response");
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
       } else if (error.request) {
-        console.log("error.request");
+        console.log("error in request");
         
-        console.log(error.request.toJSON());
+        console.log(error.request);
+        setName(error.request);
       } else {
-        console.log("other eror");
+        console.log("other error");
         console.log('Error', error.message.toJSON());
       }
       // console.log(error.config);
@@ -44,6 +54,12 @@ const Home = () => {
     <div className="login">
       <form className="login__form" onSubmit={(e) => handleSubmit(e)}>
         <h1>Login here 🚪</h1>
+        <input
+          type="name"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
         <input
           type="name"
           placeholder="Name"
